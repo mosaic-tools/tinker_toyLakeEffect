a36dd8af649e0b1a4879834fc70fc4d88dbddcf9
joneslabND <sjones20@nd.edu>
2019-10-23 15:04:37 -0400

    initial commit of network configuration manuscript analyses

---
+++ tinkertoyLakeEffect.R
@@ -0,0 +1,79 @@
+# script to use tinker_toy model and Savannah river to explore influence network structure (location of lakes, number of lakes, etc.) on biogeochemistry
+
+# source tinker_toy functions
+source("tinkerToy_analytical.R")
+
+# load stream-only network (Savannah river out of NHD)
+NHDout=read.csv("savannah_river_no1_v2.csv",stringsAsFactors=FALSE)
+
+# scaling relationship between discharge and width from Raymond et al. 2012 "scaling the gas transfer velocity and hydraulic geometry on streams and small rivers"
+a=12.88
+b=0.42
+
+# scaling relationship between discharge and depth from Lauren
+c=0.408
+d=0.294
+
+# reachNumber arbitrary ordering from NHD output
+# reachID from NHD (COMID)
+# length from NHD (LENGTHKM)
+# width from hydraulic geometry
+# depth from (discharge/velocity)/(width from hydraulic geometry)
+# localArea from NHD (AreaSqKM)
+# localPET standardized across catchment and 
+#     * google says 35-year average annual precip is 43.28 in or 1.099 m
+#     * ET = 1 m/yr best guess from maps on google
+#     * daily PET is (1.099-1)/365 = 0.0003 m/d
+#     Annual Estimates of Recharge, Quick-Flow Runoff, and Evapotranspiration for the Contiguous U.S. Using Empirical Regression Equations
+#     Reitz et al. 2017
+# localCin standardized across catchment and arbitrary
+# connect derived from information provided by John Gardner (fromCOMID)
+# order from NHD for inserting lakes
+
+#  convert NHD output to network dataframe for  use in tinker_toy; note  we add order and lake fields for use with model experiments
+network=data.frame(reachNumber=1:nrow(NHDout), 
+                  reachID=NHDout$COMID, 
+                  length=NHDout$LENGTHKM*1000, 
+                  width=a*(NHDout$QE_MA*0.0283)^b, 
+                  depth=c*(NHDout$QE_MA*0.0283)^d, 
+                  localArea=NHDout$AreaSqKM*1e6, 
+                  localPET=0.005, 
+                  localCin=10, 
+                  d=0.1, 
+                  connect=NHDout$fromCOMID,
+                  order=NHDout$StreamOrde,
+                  lake=0)
+
+# removing some artifacts from how John created the contributing reaches
+network$connect=gsub("c(","",network$connect,fixed=TRUE)
+network$connect=gsub(")","",network$connect,fixed=TRUE)
+network$connect=gsub(" ","",network$connect,fixed=TRUE)
+network$connect=gsub(":",",",network$connect,fixed=TRUE)
+network$connect[is.na(network$connect)]=0
+
+# subtracting 1 from order because we removed first order streams
+#network$order=network$order-1
+
+# because we removed true headwater streams we need to include their contributing area
+# in the contributing area of true 2nd order (our headwater streams)
+#network$localArea[network$order==1]=NHDout$TotDASqKM[network$order==1]*1e6
+
+# sort network
+#sorted=sortNetwork(network)
+#write.csv(sorted,"SORTED_savannah_river_no1_v2.csv",row.names=FALSE)
+sorted=read.csv("SORTED_savannah_river_no1_v2.csv",header=TRUE,stringsAsFactors=FALSE)
+
+# solve equilibrium for base network
+baseProcess=solveNetwork(network=sorted)
+
+
+#*******************************#
+#   Running model experiments   #
+#*******************************#
+
+# add 40 lakes to 6th order streams
+lakes40=addRandomLake(network=sorted,order=rep(6,40),width=10000,depth=10,lake_d=0.005,verbose=TRUE)
+
+# solve equilibrium for base network
+lakes40Process=solveNetwork(network=lakes40)
+
